cfdc322578c97911f7b657cf898fa225e0c7af58
Pablo Silva <pablonicolassilvabravo@gmail.com>
2020-12-21 16:39:34 -0300

    components creation

---
+++ src/components/auth/NuevaCuenta.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NuevaCuenta = () => {
+  return <h1>NuevaCuenta</h1>;
+};
+
+export default NuevaCuenta;
---
+++ src/components/proyectos/Proyectos.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Proyectos = () => {
+  return <h1>Proyectos</h1>;
+};
+
+export default Proyectos;
