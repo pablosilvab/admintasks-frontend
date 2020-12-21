e624df7f01677f88588f85cb16f19d98c385ee90
Pablo Silva <pablonicolassilvabravo@gmail.com>
2020-12-21 17:48:23 -0300

    layout login y nueva cuenta

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  const onChange = () => {};
+  const [usuario, guardarUsuario] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = usuario;
+
+  const onChange = (e) => {
+    guardarUsuario({
+      ...usuario,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = (e) => {};
 
   return (
     <div className="form-usuario">
@@ -15,17 +30,19 @@ const Login = () => {
               id="email"
               name="email"
               placeholder="Tu Email"
+              value={email}
               onChange={onChange}
             />
           </div>
 
           <div className="campo-form">
-            <label htmlFor="password">Email</label>
+            <label htmlFor="password">Password</label>
             <input
               type="password"
               id="password"
               name="password"
               placeholder="Tu Password"
+              value={password}
               onChange={onChange}
             />
           </div>
@@ -38,6 +55,11 @@ const Login = () => {
             />
           </div>
         </form>
+
+        <Link to={"/nueva-cuenta"} className="enlace-cuenta">
+          {" "}
+          Obtener Cuenta
+        </Link>
       </div>
     </div>
   );
--- src/components/auth/NuevaCuenta.js
+++ src/components/auth/NuevaCuenta.js
@@ -1,7 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const NuevaCuenta = () => {
-  return <h1>NuevaCuenta</h1>;
+  const [usuario, guardarUsuario] = useState({
+    nombre: "",
+    email: "",
+    password: "",
+    confirmar: "",
+  });
+
+  const { nombre, email, password, confirmar } = usuario;
+
+  const onChange = (e) => {
+    guardarUsuario({
+      ...usuario,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = (e) => {};
+
+  return (
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1>Crear una cuenta</h1>
+        <form>
+          <div className="campo-form">
+            <label htmlFor="nombre">Nombre</label>
+            <input
+              type="text"
+              id="nombre"
+              name="nombre"
+              placeholder="Tu Nombre"
+              value={nombre}
+              onChange={onChange}
+            />
+          </div>
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Tu Email"
+              value={email}
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Tu Password"
+              value={password}
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="campo-form">
+            <label htmlFor="confirmar">Confirmar Password</label>
+            <input
+              type="password"
+              id="confirmar"
+              name="confirmar"
+              placeholder="Repite tu Password"
+              value={confirmar}
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="campo-form">
+            <input
+              type="submit"
+              className="btn btn-primario btn-block"
+              value="Registrarme"
+            />
+          </div>
+        </form>
+
+        <Link to={"/"} className="enlace-cuenta">
+          {" "}
+          Volver
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default NuevaCuenta;
